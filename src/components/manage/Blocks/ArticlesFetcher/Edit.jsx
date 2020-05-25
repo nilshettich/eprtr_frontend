371f6ec8c8cf55589c55c24185961289781facf7
Your Name <andrei.ggr@gmail.com>
2020-05-25 17:36:11 +0300

    fetching ok fix

--- src/components/manage/Blocks/ArticlesFetcher/Edit.jsx
+++ src/components/manage/Blocks/ArticlesFetcher/Edit.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { injectIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import { Grid } from 'semantic-ui-react';
@@ -14,53 +14,23 @@ import schema from './schema';
 
 const SparqlClient = require('sparql-http-client')
 
-//https://cors-anywhere.herokuapp.com/http://example.com
-
-const endpointUrl = 'https://cors-anywhere.herokuapp.com/http://semantic.eea.europa.eu/sparql'
-const query = `PREFIX eea: <http://www.eea.europa.eu/ontologies.rdf#>
-PREFIX amp:<http://rdfdata.eionet.europa.eu/amp/ontology/> 
-PREFIX prod: <http://www.eea.europa.eu/portal_types/Article#>
-PREFIX dc: <http://purl.org/dc/terms/>
-PREFIX schema: <http://schema.org/>
-
-SELECT DISTINCT 
-?subject as ?uri
-?title
-?published
-?translation_of_uri
-?description
-?type_uri
-WHERE {
-  ?subject a prod:Article .
-  ?subject dc:title ?title .
-  ?subject dc:issued ?published .
-  OPTIONAL { ?subject eea:isTranslationOf ?translation_of_uri } .
-  OPTIONAL { ?subject dc:description ?description } .
-  ?subject a ?type_uri .
-  FILTER (?type_uri = prod:Article)
-}`
-
 const Edit = (props) => {
 
+  const [articleList, setArticleList] = useState("")
+
   useEffect(() => {
-    //do stuff on data change
-
-
+    if (props.data.endpoint && props.data.request && articleList !== props.data.articles) {
+      handleFetch()
+    }
   }, [props.data])
 
-  const getItems = () => {
-    if (props.data.endpoint && props.data.request) {
-      handleFetch()
-    } else { console.log('Nothing to query. No query.') }
-  }
 
   const handleFetch = async () => {
-    const endpoint = props.data.endpoint
+    const endpointUrl = props.data.endpoint
     const request = props.data.request
 
-
     const client = new SparqlClient({ endpointUrl })
-    const stream = await client.query.select(query)
+    const stream = await client.query.select(request)
 
     var fullItems = []
 
@@ -70,16 +40,46 @@ const Edit = (props) => {
 
     console.log(fullItems)
 
+    setArticleList(fullItems)
+
+    props.onChangeBlock(props.block, {
+      ...props.data,
+      articles: fullItems
+    });
+
     stream.on('error', err => {
       console.error(err)
     })
   }
 
+  const hasQueryData = props.data.endpoint && props.data.request
+
+  const articles = props.data.articles ? props.data.articles : ''
+
   return (
     <Grid columns={1}>
-      the fetcher edit
-
-      <button onClick={() => getItems()}>Fetch</button>
+      {hasQueryData && !articles &&
+        <p>Loading..</p>
+      }
+      {hasQueryData && articles &&
+        articles.slice(0, 10).map(article =>
+          <div className="article-list-row">
+            <img className="article-img" src={`https://readreidread.files.wordpress.com/2011/09/yellow_tree1.jpg?w=998&h=624`} />
+            <Grid.Column>
+              <p className="article-title">
+                {article.title.value}</p>
+              <p class="article-date">{article.published.value}</p>
+              <p className="article-description">{article.description.value}</p>
+              <a className="read-article" target="_blank" href={article.uri.value}>
+                READ ARTICLE
+              </a>
+            </Grid.Column>
+          </div>
+        )
+      }
+      {!hasQueryData &&
+        <p>Use Sidebar to set Endpoint and Query Data.</p>
+      }
       <SidebarPortal selected={props.selected}>
         <BlockEditForm
           schema={schema}
